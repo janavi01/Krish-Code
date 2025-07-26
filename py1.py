977af468b33e7926edee93359d78e5d5bd9b5e2a
janavi01 <janavib592@gmail.com>
2025-07-26 11:24:49 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,9 @@
+n=10
+x=10
+x=n*(n+1)/2
+print("output of above expression is: ",x)
+x=(n*(n+1)/20)
+print("output of above expression is: ",x)
+x=n*(n+1)/2
+print("output of above expression is: ",x)
+   
